3a667a12d5c9792dbca4fe6c6b321961a97dd54c
layerzerolabs <info@l0l.org.uk>
2013-11-08 21:52:41 +0000

    Added LOST_CONNECTION_OR_BAD_DATA for when we don't know which

--- Sensor.h
+++ Sensor.h
@@ -10,9 +10,10 @@ class Sensor {
 	virtual int read();
         float value;
         char* name;
+        const int OK = 0;
         const int LOST_CONNECTION = 1;
         const int BAD_DATA = 2;
-        const int OK = 0;
+        const int LOST_CONNECTION_OR_BAD_DATA = 3;
         const float arduinoVoltage = 4.93;
 };
 
